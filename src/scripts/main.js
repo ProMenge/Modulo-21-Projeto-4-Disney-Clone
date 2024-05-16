b2025248e71ef9aa7e3e5ff9b421f9b417f9f8f5
Fred Menge <mengeprofissional@gmail.com>
2024-05-16 11:46:56 -0300

    Finalização da funcionalidade de troca de abas

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,10 +6,23 @@ document.addEventListener('DOMContentLoaded', function () {
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function (botao) {
             const abaAlvo = botao.target.dataset.tabButton;
-
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`)
+            escondeTodasAbas();
+            aba.classList.add('shows__list--is-active');
+            removeBotaoAtivo();
+            botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
 })
+
+function removeBotaoAtivo() {
+
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('shows__tabs__button--is-active')
+    }
+}
 
 function escondeTodasAbas() {
 
@@ -17,6 +30,8 @@ function escondeTodasAbas() {
 
     for (let i = 0; i < tabsContainer.length; i++) {
 
-        tabsContainer
+        tabsContainer[i].classList.remove('shows__list--is-active')
+        console.log('removendo');
+        console.log(tabsContainer[i])
     }
 }
